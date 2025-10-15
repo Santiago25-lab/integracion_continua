f60513bf76188d697d62c45fb1d9cfc118a753ae
Santiago2509 <santiagourubina2509@gmail.com>
2025-10-15 12:06:33 -0500

    introduciendo error clorpisker

--- src/components/ColorPicker.test.tsx
+++ src/components/ColorPicker.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import ColorPicker from './ColorPicker'; // Asegúrate de que la ruta sea correcta
+import ColorPisker from './ColorPicker'; // Asegúrate de que la ruta sea correcta
 import '@testing-library/jest-dom'; // Para usar las aserciones como toHaveTextContent
 
 // Simular localStorage en los tests
