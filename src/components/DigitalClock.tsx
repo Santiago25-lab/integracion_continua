5193896589b88688edba375b5a9a59e5b478edb9
Santiago2509 <santiagourubina2509@gmail.com>
2025-10-15 12:10:04 -0500

    introduciedo error secodss

--- src/components/DigitalClock.tsx
+++ src/components/DigitalClock.tsx
@@ -16,7 +16,7 @@ const DigitalClock: React.FC = () => {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
+    const secondss = String(date.getSeconds()).padStart(2, '0');
     return `${hours}:${minutes}:${seconds}`;
   }
 
